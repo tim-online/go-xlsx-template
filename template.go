053eca61970d469f063d23ba46a59eb05ce11be2
Leon Bogaert <leon@tim-online.nl>
2016-12-12 15:05:38 +0100

    Basic functionality working

--- template.go
+++ template.go
@@ -5,13 +5,15 @@ import (
 	"bytes"
 	"fmt"
 	"log"
+	"regexp"
 	"strconv"
+	"strings"
 	ttmpl "text/template"
 
 	"github.com/tealeg/xlsx"
 )
 
-type cellMapper func(*xlsx.Cell) error
+type cellMapper func(rowI int, colI int, cell *xlsx.Cell) error
 
 type Template struct {
 	sheet  *xlsx.Sheet
@@ -26,14 +28,15 @@ func New(sheet *xlsx.Sheet) *Template {
 }
 
 func (t *Template) Execute(data interface{}) error {
-	err := t.mapCells(sheet, func(cell *xlsx.Cell) error {
-		return expandRange(cell, data)
+	err := t.mapCells(func(rowI int, colI int, cell *xlsx.Cell) error {
+		return t.expandRanges(cell, data)
 	})
 	if err != nil {
+		return nil
 		return err
 	}
 
-	err = t.mapCells(sheet, func(cell *xlsx.Cell) error {
+	err = t.mapCells(func(rowI int, colI int, cell *xlsx.Cell) error {
 		return t.applyData(cell, data)
 	})
 	if err != nil {
@@ -43,21 +46,30 @@ func (t *Template) Execute(data interface{}) error {
 	return nil
 }
 
-func (t *Template) mapCells(sheet *xlsx.Sheet, fn cellMapper) error {
-	log.Printf("%d rows\n", len(sheet.Rows))
-	log.Printf("%d columns\n", len(sheet.Cols))
+func (t *Template) mapCol(colI int, fn cellMapper) error {
+	for rowI, row := range t.sheet.Rows {
+		// cell doesn't exist in row
+		if colI > (len(row.Cells) - 1) {
+			return nil
+		}
 
-	for rowI, row := range sheet.Rows {
-		log.Printf("%d cells\n", len(row.Cells))
+		cell := row.Cells[colI]
+		err := fn(rowI, colI, cell)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (t *Template) mapCells(fn cellMapper) error {
+	for rowI, row := range t.sheet.Rows {
 		for colI, cell := range row.Cells {
-			if cell.Value == "" {
-				continue
-			}
-
 			t.curRow = rowI
 			t.curCol = colI
 
-			err := fn(cell)
+			err := fn(rowI, colI, cell)
 			if err != nil {
 				address := fmt.Sprintf("%d/%d", rowI, colI)
 				return fmt.Errorf("error in template (cell %s): %s", address, err)
@@ -68,13 +80,59 @@ func (t *Template) mapCells(sheet *xlsx.Sheet, fn cellMapper) error {
 	return nil
 }
 
-func expandRange(cell *xlsx.Cell, data interface{}) error {
+func (t *Template) expandRanges(cell *xlsx.Cell, data interface{}) error {
+	// cell is empty: do nothing
+	if cell.Value == "" {
+		return nil
+	}
+
+	if isColExpander(cell) == false {
+		return nil
+	}
+
+	values := splitCellValue(cell.Value)
+	for i, _ := range values {
+		value := values[i]
+		tmpl, err := ttmpl.New("tmpl_range").
+			Funcs(ttmpl.FuncMap{
+				"col_range": t.colRange,
+			}).
+			Option("missingkey=error").
+			Parse(value)
+		if err != nil {
+			return err
+		}
+
+		// at this point col_range is removed and the columns are copied
+
+		var b bytes.Buffer
+		w := bufio.NewWriter(&b)
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			return err
+		}
+
+		w.Flush()
+		newValue := b.String()
+
+		// if values are the same: do nothing
+		if newValue == value {
+			continue
+		}
+
+		values[i] = newValue
+	}
+
+	cell.Value = strings.Join(values, "")
 	return nil
 }
 
 func (t *Template) applyData(cell *xlsx.Cell, data interface{}) error {
+	if cell.Value == "" {
+		return nil
+	}
+
 	tmpl, err := ttmpl.New("tmpl_cell").
-		Funcs(t.funcMap()).
 		Option("missingkey=error").
 		Parse(cell.Value)
 	if err != nil {
@@ -83,21 +141,14 @@ func (t *Template) applyData(cell *xlsx.Cell, data interface{}) error {
 
 	switch cell.Type() {
 	case xlsx.CellTypeString:
-		fmt.Println("celltype string")
 	case xlsx.CellTypeFormula:
-		fmt.Println("celltype formula")
 	case xlsx.CellTypeNumeric:
-		fmt.Println("celltype numeric")
 	case xlsx.CellTypeBool:
-		fmt.Println("celltype bool")
 	case xlsx.CellTypeInline:
-		fmt.Println("celltype inline")
 	case xlsx.CellTypeError:
-		fmt.Println("celltype error")
 	case xlsx.CellTypeDate:
-		fmt.Println("celltype date")
 	case xlsx.CellTypeGeneral:
-		fmt.Println("celltype general")
+	default:
 	}
 
 	var b bytes.Buffer
@@ -106,17 +157,14 @@ func (t *Template) applyData(cell *xlsx.Cell, data interface{}) error {
 	if err != nil {
 		return err
 		// missing data: silently fail
-		fmt.Printf("after (1): %s\n", cell.Value)
 		return nil
 	}
 
-	fmt.Printf("before: %s\n", cell.Value)
 	w.Flush()
 	newValue := b.String()
 
 	// if values are the same: do nothing
 	if newValue == cell.Value {
-		fmt.Printf("after (2): %s\n", cell.Value)
 		return nil
 	}
 
@@ -124,29 +172,132 @@ func (t *Template) applyData(cell *xlsx.Cell, data interface{}) error {
 	f, err := strconv.ParseFloat(newValue, 64)
 	if err == nil {
 		cell.SetFloat(f)
-		fmt.Printf("after (3): %s\n", cell.Value)
 		return nil
 	}
 
 	// treat new value as a string
 	cell.Value = newValue
-	fmt.Printf("after (4): %s\n", cell.Value)
 
 	return nil
 }
 
-func (t *Template) colRange(data interface{}) string {
-	// can't loop over %type, type must be slice or map
-	fmt.Println("--------------------------------------")
-	fmt.Println(data)
-	fmt.Println(t.curRow)
-	fmt.Println(t.curCol)
-	fmt.Println("--------------------------------------")
+func (t *Template) colRange(data []interface{}) string {
+	// add new columns
+	newColsCount := len(data) - 1
+	for i := 0; i < newColsCount; i++ {
+		t.copyColToRight(t.curCol)
+	}
+
+	// loop each element in data slice and fill corresponding coumn
+	for i := 0; i < len(data); i++ {
+		// get cells from original column
+		cells := t.colCells(t.curCol)
+
+		// get colls from added column (to the right)
+		if i > 0 {
+			cells = t.colCells(t.curCol + 1)
+		}
+
+		// loop each cell in column and apply data
+		for _, cell := range cells {
+			// fmt.Printf("\n")
+			// fmt.Printf("\n")
+			// fmt.Printf("%+v\n", cell.Value)
+			// fmt.Printf("%+v\n", data[i])
+			err := t.applyData(cell, data[i])
+			if err != nil {
+				log.Println(err)
+			}
+		}
+	}
+
 	return ""
 }
 
-func (t *Template) funcMap() ttmpl.FuncMap {
-	return ttmpl.FuncMap{
-		"col_range": t.colRange,
+func (t *Template) emptyColRange(data []interface{}) string {
+	return ""
+}
+
+func (t *Template) colCells(col int) []*xlsx.Cell {
+	cells := []*xlsx.Cell{}
+
+	for _, row := range t.sheet.Rows {
+		cells = append(cells, row.Cells[col])
 	}
+
+	return cells
+}
+
+func (t *Template) copyColToRight(colNumber int) {
+	cells := t.colCells(colNumber)
+	t.insertNewColAfter(colNumber)
+	newColNumber := colNumber + 1
+
+	// loop every row in column and copy value from original column
+	err := t.mapCol(newColNumber, func(rowI int, colI int, cell *xlsx.Cell) error {
+		from := cells[rowI]
+		to := cell
+		err := copyCell(from, to)
+		return err
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func copyCell(from *xlsx.Cell, to *xlsx.Cell) error {
+	switch from.Type() {
+	case xlsx.CellTypeString:
+		to.SetString(from.Value)
+	case xlsx.CellTypeFormula:
+		to.SetFormula(from.Value)
+	case xlsx.CellTypeNumeric:
+		i, err := from.Int()
+		if err != nil {
+			return err
+		}
+		to.SetInt(i)
+	case xlsx.CellTypeBool:
+		to.SetBool(from.Bool())
+	case xlsx.CellTypeInline:
+		to.Value = from.Value
+	case xlsx.CellTypeError:
+		to.Value = from.Value
+	case xlsx.CellTypeDate:
+		to.Value = from.Value
+		f, err := to.Float()
+		if err != nil {
+			return err
+		}
+		to.SetDateTimeWithFormat(f, from.NumFmt)
+	case xlsx.CellTypeGeneral:
+		to.SetValue(from.Value)
+	default:
+		to.SetValue(from.Value)
+	}
+
+	style := from.GetStyle()
+	to.SetStyle(style)
+
+	return nil
+}
+
+func (t *Template) insertNewColAfter(colNumber int) {
+	for _, row := range t.sheet.Rows {
+
+		row.Cells = append(row.Cells, &xlsx.Cell{})
+		copy(row.Cells[colNumber+1:], row.Cells[colNumber:])
+		row.Cells[colNumber+1] = &xlsx.Cell{}
+	}
+
+	t.sheet.Cols = append(t.sheet.Cols, &xlsx.Col{})
+}
+
+func splitCellValue(cell string) []string {
+	return strings.Split(cell, "\n")
+}
+
+func isColExpander(cell *xlsx.Cell) bool {
+	return regexp.MustCompile(`{{\s?col_range.*?}}`).MatchString(cell.Value)
 }
--- template_test.go
+++ template_test.go
@@ -1,24 +1,188 @@
 package template
 
 import (
+	"fmt"
 	"log"
 	"testing"
 
 	"github.com/tealeg/xlsx"
 )
 
-var (
-	file  *xlsx.File
-	sheet *xlsx.Sheet
-)
-
 func setup() {
-	sheet = getSheet()
 }
 
-func getSheet() *xlsx.Sheet {
+func teardown() {
+}
+
+func newSheet(cells [][]string) *xlsx.Sheet {
+	sheet := &xlsx.Sheet{
+		Name:     "test",
+		File:     nil,
+		Selected: true,
+	}
+
+	for _, rowCells := range cells {
+		row := sheet.AddRow()
+		for _, value := range rowCells {
+			cell := row.AddCell()
+			cell.SetValue(value)
+		}
+	}
+	return sheet
+}
+
+func compareSheets(sheet1 *xlsx.Sheet, sheet2 *xlsx.Sheet) error {
+	for rowI, row := range sheet1.Rows {
+		for colI, _ := range row.Cells {
+			cell1 := sheet1.Cell(rowI, colI)
+			cell2 := sheet2.Cell(rowI, colI)
+			if cell1.Value != cell2.Value {
+				return fmt.Errorf("cells in row %d, column %d are not the same ('%s' vs '%s')", rowI, colI, cell1.Value, cell2.Value)
+			}
+		}
+	}
+
+	return nil
+}
+
+type simpleTest struct {
+	cell string
+	data interface{}
+	want string
+	err  bool
+}
+
+func TestSimple(t *testing.T) {
+	setup()
+	defer teardown()
+
+	tables := []simpleTest{
+		simpleTest{"", []string{}, "", false},
+		simpleTest{"Should be the same", []string{}, "Should be the same", false},
+		simpleTest{"{{ .test }}", map[string]string{"test": "test"}, "test", false},
+		simpleTest{"Should do nothing", map[string]string{"test": "test"}, "Should do nothing", false},
+		simpleTest{"{{ .test }}", map[string]string{}, "{{ .test }}", true},
+		simpleTest{"{{ }}", map[string]string{}, "{{ }}", true},
+		simpleTest{"0", map[string]string{}, "0", false},
+		simpleTest{"0.0", map[string]string{}, "0.0", false},
+	}
+
+	for i, table := range tables {
+		cell := &xlsx.Cell{}
+		cell.SetValue(table.cell)
+		tmpl := &Template{}
+		err := tmpl.applyData(cell, table.data)
+		if table.err == false && err != nil {
+			t.Error(err)
+			continue
+		}
+		if table.err == true {
+			if err == nil {
+				t.Errorf("Wanted an error for test %d, got nothing", i)
+			}
+		}
+
+		want := table.want
+		got := cell.Value
+
+		if want != got {
+			t.Errorf("got '%s', want '%s' for test %d", got, want, i)
+		}
+	}
+}
+
+func TestSimpleColRangeExpand(t *testing.T) {
+	setup()
+	defer teardown()
+
+	cells := [][]string{
+		[]string{"header 1", "header 2", "{{col_range .cols}}header 3", "header 4"},
+		[]string{"", "static 1", "row1", "static 3"},
+		[]string{"", "static 2", "row2", "static 4"},
+	}
+
+	got := newSheet(cells)
+
+	data := map[string][]interface{}{
+		"cols": []interface{}{
+			map[string]interface{}{},
+			map[string]interface{}{},
+		},
+	}
+
+	tmpl := New(got)
+	err := tmpl.Execute(data)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(got.Cols) != 5 {
+		t.Errorf("Wanted 5 cols, got %d cols", len(got.Cols))
+	}
+
+	want := newSheet([][]string{
+		[]string{"header 1", "header 2", "header 3", "header 3", "header 4"},
+		[]string{"", "static 1", "row1", "row1", "static 3"},
+		[]string{"", "static 2", "row2", "row2", "static 4"},
+	})
+
+	err = compareSheets(got, want)
+	if err != nil {
+		t.Error(err)
+	}
+
+}
+
+func TestColRangeExpand(t *testing.T) {
+	setup()
+	defer teardown()
+
+	cells := [][]string{
+		[]string{"header 1", "header 2", "{{col_range .cols}}header 3", "header 4"},
+		[]string{"", "static 1", "{{ .row1 }}", "static 3"},
+		[]string{"", "static 2", "{{ .row2 }}", "static 4"},
+	}
+
+	got := newSheet(cells)
+
+	data := map[string][]interface{}{
+		"cols": []interface{}{
+			map[string]interface{}{
+				"row1": "dynamic 1",
+				"row2": "dynamic 2",
+			},
+			map[string]interface{}{
+				"row1": "dynamic 3",
+				"row2": "dynamic 4",
+			},
+		},
+	}
+
+	tmpl := New(got)
+	err := tmpl.Execute(data)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(got.Cols) != 5 {
+		t.Errorf("Wanted 5 cols, got %d cols", len(got.Cols))
+	}
+
+	want := newSheet([][]string{
+		[]string{"header 1", "header 2", "header 3", "header 3", "header 4"},
+		[]string{"", "static 1", "dynamic 1", "dynamic 3", "static 3"},
+		[]string{"", "static 2", "dynamic 2", "dynamic 4", "static 4"},
+	})
+
+	err = compareSheets(got, want)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestSchipper(t *testing.T) {
 	var err error
-	file, err = xlsx.OpenFile("testdata/template.xlsx")
+	file, err := xlsx.OpenFile("testdata/template.xlsx")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -28,67 +192,76 @@ func getSheet() *xlsx.Sheet {
 	}
 
 	libreOfficeFillFix(file)
-
-	return file.Sheets[0]
-}
-
-func teardown() {
-}
-
-func TestNewTemplate(t *testing.T) {
-	setup()
-	defer teardown()
+	sheet := file.Sheets[0]
 
 	tmpl := New(sheet)
 
-	if tmpl == nil {
-		t.Errorf("Template is nil")
-	}
-}
-
-func TestSimple(t *testing.T) {
-	setup()
-	defer teardown()
-
-	tmpl := New(sheet)
-
-	data := map[string]interface{}{
-		"placeholder1":  "Value 1",
-		"placeholder2":  "Value 2",
-		"aangifte_jaar": 10.0,
-		"offices": map[string]string {
-			"1": "een",
-			"2": "twee",
+	data := map[string][]interface{}{
+		"offices": []interface{}{
+			map[string]interface{}{
+				"placeholder1":  "Value 1",
+				"placeholder2":  "Value 2",
+				"aangifte_jaar": 10.0,
+				"office_id":                                        "TO",
+				"office_name":                                      "Tim_online",
+				"aangifte_naam":                                    "Test",
+				"aangifte_periode":                                 "12/04",
+				"aangifte_code":                                    "test",
+				"ob_nummer":                                        "ob-1204",
+				"omzet_hoog":                                       120.0,
+				"btw_hoog":                                         110.0,
+				"omzet_laag":                                       90.0,
+				"btw_laag":                                         30.0,
+				"prive_gebruik":                                    0.0,
+				"omzet_belast":                                     0.0,
+				"omzet_verlegd":                                    0.0,
+				"btw_verlegd":                                      0.0,
+				"levering_naar_landen_buiten_de_eu":                0.0,
+				"omzet_binnen_eu":                                  0.0,
+				"omzet_leveringen_uit_landen_buiten_eu":            0.0,
+				"btw_leveringen_uit_landen_buiten_eu":              0.0,
+				"levering_van_goederen_uit_landen_binnen_eu_omzet": 0.0,
+				"levering_van_goederen_uit_landen_binnen_eu_btw":   0.0,
+				"verschuldigde_btw":                                0.0,
+				"voorbelasting":                                    0.0,
+				"subtotaal":                                        0.0,
+				"totaal_te_betalen":                                0.0,
+			},
+			map[string]interface{}{
+				"placeholder1":  "Value 1",
+				"placeholder2":  "Value 2",
+				"aangifte_jaar": 10.0,
+				"office_id":                                        "TO",
+				"office_name":                                      "Tim_online",
+				"aangifte_naam":                                    "Test",
+				"aangifte_periode":                                 "12/04",
+				"aangifte_code":                                    "test",
+				"ob_nummer":                                        "ob-1204",
+				"omzet_hoog":                                       120.0,
+				"btw_hoog":                                         110.0,
+				"omzet_laag":                                       90.0,
+				"btw_laag":                                         30.0,
+				"prive_gebruik":                                    0.0,
+				"omzet_belast":                                     0.0,
+				"omzet_verlegd":                                    0.0,
+				"btw_verlegd":                                      0.0,
+				"levering_naar_landen_buiten_de_eu":                0.0,
+				"omzet_binnen_eu":                                  0.0,
+				"omzet_leveringen_uit_landen_buiten_eu":            0.0,
+				"btw_leveringen_uit_landen_buiten_eu":              0.0,
+				"levering_van_goederen_uit_landen_binnen_eu_omzet": 0.0,
+				"levering_van_goederen_uit_landen_binnen_eu_btw":   0.0,
+				"verschuldigde_btw":                                0.0,
+				"voorbelasting":                                    0.0,
+				"subtotaal":                                        0.0,
+				"totaal_te_betalen":                                0.0,
+			},
 		},
-		"office_id": "TO",
-		"office_name": "Tim_online",
-		"aangifte_naam": "Test",
-		"aangifte_periode": "12/04",
-		"aangifte_code": "test",
-		"ob_nummer": "ob-1204",
-		"omzet_hoog": 120.0,
-		"btw_hoog": 110.0,
-		"omzet_laag": 90.0,
-		"btw_laag": 30.0,
-		"prive_gebruik": 0.0,
-		"omzet_belast": 0.0,
-		"omzet_verlegd": 0.0,
-		"btw_verlegd": 0.0,
-		"levering_naar_landen_buiten_de_eu": 0.0,
-		"omzet_binnen_eu": 0.0,
-		"omzet_leveringen_uit_landen_buiten_eu": 0.0,
-		"btw_leveringen_uit_landen_buiten_eu": 0.0,
-		"levering_van_goederen_uit_landen_binnen_eu_omzet": 0.0,
-		"levering_van_goederen_uit_landen_binnen_eu_btw": 0.0,
-		"verschuldigde_btw": 0.0,
-		"voorbelasting": 0.0,
-		"subtotaal": 0.0,
-		"totaal_te_betalen": 0.0,
 	}
 
-	err := tmpl.Execute(data)
+	err = tmpl.Execute(data)
 	if err != nil {
-		t.Error(err)
+		panic(err)
 	}
 
 	file.Save("test.xlsx")
